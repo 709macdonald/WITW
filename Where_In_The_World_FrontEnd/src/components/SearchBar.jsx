81fa7eaa6f9b8c4ead3e0797db9177fe58036593
709macdonald <709macdonald@gmail.com>
2025-02-24 06:02:35 -0330

    fixed CSS spacing

--- Where_In_The_World_FrontEnd/src/components/SearchBar.jsx
+++ Where_In_The_World_FrontEnd/src/components/SearchBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function SearchBar() {
   return (
-    <div>
+    <div className="searchDiv">
       <h3>Enter Your Current Location</h3>
       <input placeholder="Whats on the other side of..."></input>
       <button>Search</button>
